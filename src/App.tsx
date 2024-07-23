6bbd265751e4f29358248f2b618c1f13090b13d4
Matthew McCall <m-mccall+git@outlook.com>
2024-07-23 15:05:47 -0400

    Update default React app boilerplate to OASIS Web UI
    The default React App has been replaced with OASIS Web app layout. This includes transition from a simple counter application to a layout with input fields and display areas. Bootstrap CSS and components were added to improve UI, and the previous static CSS was removed. This is an iteration towards creating a complete web app for Algebraic solutions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Button, Container, Form, InputGroup, Stack} from "react-bootstrap";
+import {useRef, useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [entries, setEntries] = useState<string[]>([]);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const bottomRef = useRef<HTMLDivElement>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function onEntry() {
+        if (!inputRef.current?.value) return;
+
+        setEntries([...entries, inputRef.current.value])
+        bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+
+    return (
+        <>
+            <Container>
+                <Stack className={"min-vh-100 py-5"}>
+                    <div>
+                        <h1 className={"fw-semibold"}>OASIS Web</h1>
+                        <p className={"lead"}>Open Algebra Software for Inferring Solutions</p>
+                    </div>
+                    <div className={"flex-grow-1"}>
+                        <Stack gap={3}>
+                            {entries.map((entry, index) => (
+                                index % 2 === 0
+                                    ? <div className={"align-self-end bg-secondary-subtle rounded p-3"} key={index}>{entry}</div>
+                                    : <div className={"align-self-start bg-primary-subtle rounded p-3"} key={index}>{entry}</div>
+                            ))}
+                        </Stack>
+                    </div>
+                    <div className={"pt-5"}>
+                        <InputGroup className="mb-3">
+                            <Form.Control ref={inputRef} placeholder="Enter an expression..."/>
+                            <Button
+                                variant="primary"
+                                id="button-addon2"
+                                onClick={onEntry}
+                            >Submit</Button>
+                        </InputGroup>
+                    </div>
+                </Stack>
+            </Container>
+            <div ref={bottomRef} />
+        </>
+    )
 }
 
 export default App
