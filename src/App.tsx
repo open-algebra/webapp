6cb4642948c6a968d24e907516f5885040f53f7c
Matthew McCall <m-mccall+git@outlook.com>
2024-07-25 02:34:58 -0400

    Add Help Modal and Update Layout Structure
    Introduce a Help modal with guidance on using OASIS. Also, include layout enhancements such as additional buttons, a warning alert, and minor style improvements to ensure a better user experience.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import OasisC from './OasisC.mjs'
-import {Button, Container, Form, InputGroup, Spinner, Stack} from "react-bootstrap";
+import {Alert, Button, Container, Form, InputGroup, Modal, Spinner, Stack} from "react-bootstrap";
 import {FormEvent, useEffect, useRef, useState} from "react";
 
 interface HistoryEntry {
@@ -17,6 +17,7 @@ function App() {
     const inputRef = useRef<HTMLInputElement>(null);
     const [oasis, setOasis] = useState(null);
     const [appState, setAppState] = useState<AppState>({history: [], currentEntry: 0});
+    const [showHelp, setShowHelp] = useState(false);
 
     function addToHistory(query: string, response: string) {
         setAppState({history: [...appState.history, {query, response}], currentEntry: 0});
@@ -62,60 +63,90 @@ function App() {
         )
     }
 
+    function closeHelp() {
+        setShowHelp(false)
+    }
+
     return (
-        <Stack className={"min-vh-100"}>
-            <div className="py-3 bg-body-tertiary border-bottom">
-                <Container>
-                    <h1 className={"fw-semibold"}>OASIS Web</h1>
-                    <p className={"lead"}>Open Algebra Software for Inferring Solutions</p>
-                </Container>
-            </div>
-            <div className={"flex-grow-1 py-3"}>
-                <Container>
-                    <Stack gap={3}>
-                        {appState.history.map(({query, response}, index) => (
-                            <Stack gap={3} key={index}>
-                                <div className={"align-self-end bg-secondary-subtle rounded-5 p-3"}>
-                                    <math display={"block"}
-                                          dangerouslySetInnerHTML={{__html: query}}></math>
-                                </div>
-                                <div className={"align-self-start bg-primary-subtle rounded-5 p-3"}>
-                                    <math display={"block"}
-                                          dangerouslySetInnerHTML={{__html: response}}></math>
-                                </div>
-                            </Stack>
-                        ))}
-                        {
-                            (appState.currentEntry && oasis) === 0
-                                ? null
-                                : <div className={"align-self-end bg-secondary-subtle rounded-5 p-3"}>
-                                    <math display={"block"}
-                                          dangerouslySetInnerHTML={{__html: oasis.ccall('Oa_ExpressionToMathMLStr', 'string', ['number'], [appState.currentEntry])}}></math>
-                                </div>
-                        }
-                    </Stack>
-                </Container>
-            </div>
-            <div className={"sticky-bottom bg-body-tertiary border-top"}>
-                <Container>
-                    <Form className={"my-3"} onSubmit={onEntry}>
-                        <InputGroup>
-                            <Form.Control
-                                ref={inputRef}
-                                placeholder="Enter an expression..."
-                                onChange={() => {
-                                    inputRef.current?.value && setCurrentEntry(inputRef.current?.value)
-                                }}
-                            />
-                            <Button
-                                variant="primary"
-                                type={"submit"}
-                            >Submit</Button>
-                        </InputGroup>
-                    </Form>
-                </Container>
-            </div>
-        </Stack>
+        <>
+            <Modal show={showHelp} onHide={closeHelp}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Help</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className={"pb-3"}>
+                        Welcome to OASIS! To get started, type in any expression such as "2x+3x". Oasis automatically
+                        recognizes the variables and is able to add them for you. Some variable names are reserved, such as
+                        "i" for imaginary numbers.
+                    </div>
+                    <h5>Functions</h5>
+                    Oasis also understands some functions. For instance, <code>dd(x^2,x)</code> takes derivative of x^2
+                    with respect to x. Likewise, <code>in(2x,x)</code> takes the integral of 2x with respect to x
+                    and &nbsp; <code>log(10,100)</code>
+                    takes the logarithm of 100 with a base of 10.
+                </Modal.Body>
+            </Modal>
+            <Stack className={"min-vh-100"}>
+                <div className="bg-body-tertiary border-bottom">
+                    <Container fluid className="text-center py-3">
+                        <h1 className={"fw-semibold"}>OASIS Web</h1>
+                        <p className={"lead"}>Open Algebra Software for Inferring Solutions</p>
+                        <Stack direction="horizontal" gap={2} className="justify-content-center">
+                            <a href={"https://github.com/open-algebra/Oasis"} className={"btn btn-dark"}>GitHub</a>
+                            <Button variant={"light"} onClick={() => setShowHelp(true)}
+                                    className={"border"}>Help</Button>
+                        </Stack>
+                    </Container>
+                </div>
+                <div className={"flex-grow-1 py-3"}>
+                    <Container>
+                        <Stack gap={3}>
+                            <Alert variant={"warning"}>Oasis, OasisC, and Oasis Web are still under active development.
+                                Here be dragons.</Alert>
+                            {appState.history.map(({query, response}, index) => (
+                                <Stack gap={3} key={index}>
+                                    <div className={"align-self-end bg-secondary-subtle rounded-5 p-3"}>
+                                        <math display={"block"}
+                                              dangerouslySetInnerHTML={{__html: query}}></math>
+                                    </div>
+                                    <div className={"align-self-start bg-primary-subtle rounded-5 p-3"}>
+                                        <math display={"block"}
+                                              dangerouslySetInnerHTML={{__html: response}}></math>
+                                    </div>
+                                </Stack>
+                            ))}
+                            {
+                                (appState.currentEntry && oasis) === 0
+                                    ? null
+                                    : <div className={"align-self-end bg-secondary-subtle rounded-5 p-3"}>
+                                        <math display={"block"}
+                                              dangerouslySetInnerHTML={{__html: oasis.ccall('Oa_ExpressionToMathMLStr', 'string', ['number'], [appState.currentEntry])}}></math>
+                                    </div>
+                            }
+                        </Stack>
+                    </Container>
+                </div>
+                <div className={"sticky-bottom shadow"}>
+                    <Container>
+                        <Form className={"my-3"} onSubmit={onEntry}>
+                            <InputGroup>
+                                <Form.Control
+                                    ref={inputRef}
+                                    placeholder="Enter an expression..."
+                                    onChange={() => {
+                                        inputRef.current?.value && setCurrentEntry(inputRef.current?.value)
+                                    }}
+                                />
+                                <Button
+                                    variant="primary"
+                                    type={"submit"}
+                                >Submit</Button>
+                            </InputGroup>
+                        </Form>
+                    </Container>
+                </div>
+            </Stack>
+        </>
     )
 }
 
