02a3fd6c620162f56044795970e474474cdbbcfa
Matthew McCall <m-mccall+git@outlook.com>
2024-07-25 02:05:05 -0400

    Add support for processing algebraic expressions
    Integrated OasisC module to handle algebraic expressions including preprocessing, conversion to MathML, and simplification. Updated the UI to display history of queries and results, and added spinner for loading state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,121 @@
 import './App.css'
-import {Button, Container, Form, InputGroup, Stack} from "react-bootstrap";
-import {useRef, useState} from "react";
+import OasisC from './OasisC.mjs'
+import {Button, Container, Form, InputGroup, Spinner, Stack} from "react-bootstrap";
+import {FormEvent, useEffect, useRef, useState} from "react";
+
+interface HistoryEntry {
+    query: string,
+    response: string
+}
+
+interface AppState {
+    history: HistoryEntry[]
+    currentEntry: number
+}
 
 function App() {
-    const [entries, setEntries] = useState<string[]>([]);
     const inputRef = useRef<HTMLInputElement>(null);
-    const bottomRef = useRef<HTMLDivElement>(null);
+    const [oasis, setOasis] = useState(null);
+    const [appState, setAppState] = useState<AppState>({history: [], currentEntry: 0});
 
-    function onEntry() {
-        if (!inputRef.current?.value) return;
+    function addToHistory(query: string, response: string) {
+        setAppState({history: [...appState.history, {query, response}], currentEntry: 0});
+    }
 
-        setEntries([...entries, inputRef.current.value])
-        bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+    function setCurrentEntry(input: string) {
+        if (!oasis) return;
+
+        const preprocessedInput = oasis.ccall('Oa_PreProcessInFix', 'string', ['string'], [input]);
+        const expression = oasis.ccall('Oa_FromInFix', 'number', ['string'], [preprocessedInput]);
+        setAppState({...appState, currentEntry: expression})
+    }
+
+    function onEntry(e: FormEvent) {
+        e.preventDefault();
+
+        if (inputRef.current) {
+            inputRef.current.value = '';
+        }
+
+        if (!oasis || !appState.currentEntry) return;
+
+        const queryStr = oasis.ccall('Oa_ExpressionToMathMLStr', 'string', ['number'], [appState.currentEntry])
+
+        const result = oasis.ccall('Oa_SimplifyNF', 'number', ['number'], [appState.currentEntry]);
+        const resultStr = oasis.ccall('Oa_ExpressionToMathMLStr', 'string', ['number'], [result])
+
+        addToHistory(queryStr, resultStr);
+    }
+
+    useEffect(() => {
+        async function loadOasis() {
+            const module = await OasisC();
+            setOasis(module);
+        }
+
+        loadOasis().then();
+    }, [])
+
+    if (!oasis) {
+        return (
+            <Spinner/>
+        )
     }
 
     return (
-        <>
-            <Container>
-                <Stack className={"min-vh-100 py-5"}>
-                    <div>
-                        <h1 className={"fw-semibold"}>OASIS Web</h1>
-                        <p className={"lead"}>Open Algebra Software for Inferring Solutions</p>
-                    </div>
-                    <div className={"flex-grow-1"}>
-                        <Stack gap={3}>
-                            {entries.map((entry, index) => (
-                                index % 2 === 0
-                                    ? <div className={"align-self-end bg-secondary-subtle rounded p-3"} key={index}>{entry}</div>
-                                    : <div className={"align-self-start bg-primary-subtle rounded p-3"} key={index}>{entry}</div>
-                            ))}
-                        </Stack>
-                    </div>
-                    <div className={"pt-5"}>
-                        <InputGroup className="mb-3">
-                            <Form.Control ref={inputRef} placeholder="Enter an expression..."/>
+        <Stack className={"min-vh-100"}>
+            <div className="py-3 bg-body-tertiary border-bottom">
+                <Container>
+                    <h1 className={"fw-semibold"}>OASIS Web</h1>
+                    <p className={"lead"}>Open Algebra Software for Inferring Solutions</p>
+                </Container>
+            </div>
+            <div className={"flex-grow-1 py-3"}>
+                <Container>
+                    <Stack gap={3}>
+                        {appState.history.map(({query, response}, index) => (
+                            <Stack gap={3} key={index}>
+                                <div className={"align-self-end bg-secondary-subtle rounded-5 p-3"}>
+                                    <math display={"block"}
+                                          dangerouslySetInnerHTML={{__html: query}}></math>
+                                </div>
+                                <div className={"align-self-start bg-primary-subtle rounded-5 p-3"}>
+                                    <math display={"block"}
+                                          dangerouslySetInnerHTML={{__html: response}}></math>
+                                </div>
+                            </Stack>
+                        ))}
+                        {
+                            (appState.currentEntry && oasis) === 0
+                                ? null
+                                : <div className={"align-self-end bg-secondary-subtle rounded-5 p-3"}>
+                                    <math display={"block"}
+                                          dangerouslySetInnerHTML={{__html: oasis.ccall('Oa_ExpressionToMathMLStr', 'string', ['number'], [appState.currentEntry])}}></math>
+                                </div>
+                        }
+                    </Stack>
+                </Container>
+            </div>
+            <div className={"sticky-bottom bg-body-tertiary border-top"}>
+                <Container>
+                    <Form className={"my-3"} onSubmit={onEntry}>
+                        <InputGroup>
+                            <Form.Control
+                                ref={inputRef}
+                                placeholder="Enter an expression..."
+                                onChange={() => {
+                                    inputRef.current?.value && setCurrentEntry(inputRef.current?.value)
+                                }}
+                            />
                             <Button
                                 variant="primary"
-                                id="button-addon2"
-                                onClick={onEntry}
+                                type={"submit"}
                             >Submit</Button>
                         </InputGroup>
-                    </div>
-                </Stack>
-            </Container>
-            <div ref={bottomRef} />
-        </>
+                    </Form>
+                </Container>
+            </div>
+        </Stack>
     )
 }
 
